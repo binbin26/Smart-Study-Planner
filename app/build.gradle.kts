d738a0404bc1a02ee8615bff71df14da41267bd4
binbin26 <157558221+binbin26@users.noreply.github.com>
2025-12-30 21:22:37 +0700

    Sửa lỗi FireBase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.smartstudyplanner"
+    namespace = "smart.planner"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.smartstudyplanner"
+        applicationId = "smart.planner"
         minSdk = 29
         targetSdk = 36
         versionCode = 1
